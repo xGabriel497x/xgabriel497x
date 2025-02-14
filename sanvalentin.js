ae6720f7b3684ac0045100547cf0b1dd6fd1af78
Gabriel Paucar <gabrielbenjamin497@gmail.com>
2025-02-14 11:21:48 -0800

    New page

---
+++ sanvalentin.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    document.getElementById('messageBox').style.display = 'block';
+}
+function closeMessage() {
+    document.getElementById('messageBox').style.display = 'none';
+}
